89a716d99fd6b3e99c2a6150a40caf022ccde464
Singh Pritesh <priteshkumarsingh@fxis.ai>
2025-01-30 13:01:08 +0530

    update

--- src/pages/admin/dashboard.tsx
+++ src/pages/admin/dashboard.tsx
@@ -1,14 +1,10 @@
-// import AdminHeader from "../../components/admin/header";
-// import AdminSidebar from "../../components/admin/sidebar";
 
 function AdminDashboard(){
     return(
         <div className="flex min-h-screen w-full">
-            {/*  admin sidebar */}
-            {/* <AdminSidebar /> */}
+            
             <div className="flex flex-1 flex-col">
-                {/* admin header */}
-                {/* <AdminHeader/> */}
+                
                 <main className="flex-1 flex-col bg-muted/40 p-4 md:p-6">
                     <h1>Admin Dashboard</h1>
                 </main>
